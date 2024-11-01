f08450461cba397f1ca90d0295c17931b8fc00c9
kiljw316 <kiljw316@gmail.com>
2024-11-01 14:26:51 +0900

    add first commit

---
+++ src/test/java/com/example/tokenbasedauthentication/TokenBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tokenbasedauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenBasedAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
